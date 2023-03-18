9b94e17bbff1ae4f5a476013af52e9390749c020
aleksey.krasnov <krasnov.80@mail.ru>
2023-03-18 08:35:38 +0300

    Task25 Done

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,1 +1,11 @@
 ﻿Console.Clear();
+Console.Write("Введите число 1 ");
+int number1=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2 ");
+int number2=Convert.ToInt32(Console.ReadLine());
+int square=number1;
+for (int index=2;index<=number2;index++)
+{
+    square=square*number1;
+}
+Console.WriteLine(square);
