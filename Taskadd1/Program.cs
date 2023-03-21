71c726726790e49133b2d292e88200a3f3223cba
aleksey.krasnov <krasnov.80@mail.ru>
2023-03-21 11:01:01 +0300

    Сделал положительный сдвиг

--- Taskadd1/Program.cs
+++ Taskadd1/Program.cs
@@ -1,1 +1,45 @@
 ﻿Console.Clear();
+
+void shiftArr(int[] col) //Сдвиг массива на 1 позицию
+{
+    int buffer = col[0];
+    for (int i = 0; i < col.Length - 1; i++)
+    {
+        col[i] = col[i + 1];
+    }
+    col[col.Length - 1] = buffer;
+}
+void shifArrPos(int[]col,int shift) //Сдвиг массима на N позиций
+{
+    for(int i=0;i<shift;i++)
+{
+    shiftArr(col);
+}
+}
+// int arrAdd(int count)// Создание массива из count чисел от 1 до 100
+// {
+//     int[] arr = new int[count];
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         arr[i] = new Random().Next(1, 101);
+//     }
+//     return arr[count];
+
+// }
+
+Console.Write("Введите размер массива ");
+int count=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите сдвиг ");
+int shift=Convert.ToInt32(Console.ReadLine());
+
+//int count=10;
+int[]arr=new int[count];
+for (int i = 0; i <arr.Length; i++)
+{
+    arr[i] = new Random().Next(1, 101);
+}
+// int []arr = arrAdd(10);
+Console.WriteLine($"[{String.Join(",",arr)}]");
+shifArrPos(arr,shift);
+
+Console.WriteLine($"[{String.Join(",",arr)}]");
